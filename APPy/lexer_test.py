2fbaa8846fb10dfbfa7e026db6087b36340d6a37
Alan Pierce <alangpierce@gmail.com>
2013-11-26 23:57:28 -0800

    Basic lexer for handling numbers and addition

--- APPy/lexer_test.py
+++ APPy/lexer_test.py
@@ -1,12 +1,29 @@
-__author__ = 'alangpierce'
-
 import unittest
+from lexer import create_lexer
 
 class MyTestCase(unittest.TestCase):
     def test_simple_tokens(self):
-        input_string = '''
-            5 + 3
-        '''
-        self.assertEquals(5, len(input_string))
+        input_string = '5 + 3'
+        tokens = self.get_tokens(input_string)
+        self.assert_tokens(
+            [('NUMBER', 5),
+             ('PLUS', '+'),
+             ('NUMBER', 3)],
+            input_string)
 
+    def assert_tokens(self, expected_tokens, program):
+        tokens = self.get_tokens(program)
+        self.assertEqual(expected_tokens,
+                         [(tok.type, tok.value) for tok in tokens])
+
+    def get_tokens(self, input):
+        lexer = create_lexer()
+        lexer.input(input)
+        result = []
+        while True:
+            token = lexer.token()
+            if not token:
+                break
+            result.append(token)
+        return result
 
