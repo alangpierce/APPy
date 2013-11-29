fa8155920bfa5129662556c8c3d8dbbf7a4f9e6d
Alan Pierce <alangpierce@gmail.com>
2013-11-29 00:08:25 -0800

    Add lexing for strings

--- APPy/lexer.py
+++ APPy/lexer.py
@@ -1,4 +1,6 @@
+from re import match
 from ply import lex
+from ply.lex import TOKEN
 
 tokens = (
     'NUMBER',
@@ -8,6 +10,7 @@ tokens = (
     'DIVIDEDBY',
     'LPAREN',
     'RPAREN',
+    'STRING',
 )
 
 t_PLUS = r'\+'
@@ -16,6 +19,7 @@ t_TIMES = r'\*'
 t_DIVIDEDBY = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+
 
 def t_NUMBER(t):
     r'\d+'
@@ -26,11 +30,61 @@ def t_NUMBER(t):
         t.value = 0
     return t
 
+
+# Key-value pairs of all escape characters. Each key has an implicit
+# backslash before it.
+# TODO: Implement the following from the spec:
+# \N{name} 	Character named name in the Unicode database (Unicode only)
+# \uxxxx 	Character with 16-bit hex value xxxx (Unicode only) 	(1)
+# \Uxxxxxxxx 	Character with 32-bit hex value xxxxxxxx (Unicode only) 	(2)
+# \ooo 	Character with octal value ooo 	(3,5)
+# \xhh 	Character with hex value hh 	(4,5)
+ESCAPE_MAPPINGS = [
+    ('\n', ''),
+    ('\\', '\\'),
+    ("'", "'"),
+    ('"', '"'),
+    ('a', '\a'),
+    ('b', '\b'),
+    ('f', '\f'),
+    ('n', '\n'),
+    ('r', '\r'),
+    ('t', '\t'),
+    ('v', '\v'),
+]
+
+
+def escape_string(string):
+    for (key, value) in ESCAPE_MAPPINGS:
+        string = string.replace('\\' + key, value)
+    return string
+
+
+def string_regex(delimiter):
+    return ('[uUbB]?[Rr]?' +
+           delimiter + '([^' + delimiter + r'\\]|\\.)*' + delimiter)
+
+
+@TOKEN(string_regex("'") + '|' + string_regex('"'))
+def t_STRING(t):
+    origin_string = t.value
+    delimiter = origin_string[-1]
+    delim_start = origin_string.find(delimiter)
+    string_contents = origin_string[delim_start+1:-1]
+    prefix = origin_string[0:delim_start]
+    # TODO: Unicode, bytes
+    if not match('[rR]', prefix):
+        string_contents = escape_string(string_contents)
+    t.value = string_contents
+    return t
+
+
 t_ignore = ' \t\n'
+
 
 def t_error(t):
     raise SyntaxError('Unexpected token: ' + str(t))
 
+
 def create_lexer():
     return lex.lex()
-
--- APPy/lexer_test.py
+++ APPy/lexer_test.py
@@ -9,6 +9,8 @@ times = ('TIMES', '*')
 dividedby = ('DIVIDEDBY', '/')
 lparen = ('LPAREN', '(')
 rparen = ('RPAREN', ')')
+def string(s):
+    return ('STRING', s)
 
 class LexerTest(unittest.TestCase):
     def test_simple_tokens(self):
@@ -21,16 +23,42 @@ class LexerTest(unittest.TestCase):
             '1 + 2 - 3 * 4 / 5',
             [num(1), plus, num(2), minus, num(3), times, num(4), dividedby,
              num(5)])
-        
+
     def test_parens(self):
         self.assert_tokens(
             '3*(5 + 5)',
             [num(3), times, lparen, num(5), plus, num(5), rparen])
 
+    def test_single_quote_string_literal(self):
+        self.assert_tokens(
+            "'Hello, ' + 'world!'",
+            [string('Hello, '), plus, string('world!')])
+
+    def test_single_quote_escapes(self):
+        self.assert_tokens(
+            r"'abc\''",
+            [string("abc'")])
+
+    def test_mismatched_quote(self):
+        self.assert_lex_failure("'''")
+
+    def test_double_quite_string_literal(self):
+        self.assert_tokens(
+            '"Hello, " + "world" + \'!\'',
+            [string('Hello, '), plus, string('world'), plus, string('!')])
+
+    def test_raw_strings(self):
+        self.assert_tokens(
+            r'r"abc\n"',
+            [string(r'abc\n')])
+
     def assert_tokens(self, program, expected_tokens):
         tokens = self.get_tokens(program)
         self.assertEqual(expected_tokens,
                          [(tok.type, tok.value) for tok in tokens])
+
+    def assert_lex_failure(self, program):
+        self.assertRaises(SyntaxError, self.get_tokens, program)
 
     def get_tokens(self, input):
         lexer = create_lexer()
@@ -42,6 +70,7 @@ class LexerTest(unittest.TestCase):
                 break
             result.append(token)
         return result
+
 
 if __name__ == '__main__':
     unittest.main()
